aa7a63e8fe3bf4b62277e54da53f35825848c552
Alex Brady <alexbrady1111@gmail.com>
2025-08-14 17:16:44 +0100

    main tests pulled

--- springboot-mysql-eventura/src/main/java/com/eventura/springboot_mysql_eventura/controllers/BookingController.java
+++ springboot-mysql-eventura/src/main/java/com/eventura/springboot_mysql_eventura/controllers/BookingController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/bookings")
+@CrossOrigin(origins = "*")
 public class BookingController {
     private final BookingService bookingService;
 
