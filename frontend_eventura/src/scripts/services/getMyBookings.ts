49cb4a37857e8b68327c2abbb185ba809246a80f
Alex Brady <alexbrady1111@gmail.com>
2025-08-14 12:02:29 +0100

    fixing structure issues within the code

--- frontend_eventura/src/scripts/services/getMyBookings.ts
+++ frontend_eventura/src/scripts/services/getMyBookings.ts
@@ -1,14 +1,16 @@
-import type {MyBookings} from "../models/myBookings"
-import { userId } from "../models/userState";
+import type { MyBookings } from "../models/myBookings";
+// import { userId } from "../models/userState";
 
-export const fetchData = async () :Promise<MyBookings>=> {
+export const fetchData = async (): Promise<MyBookings> => {
     try {
-        const response = await fetch(`http://localhost:8080/api/bookings/mybookings/1`);
+        const response = await fetch(
+            `http://localhost:8080/api/bookings/mybookings/1`
+        );
         if (response.status !== 200) {
             throw new Error("Something went wrong!");
         }
-        const data: MyBookings = await response.json();;
-        console.log("Fetched data",data);
+        const data: MyBookings = await response.json();
+        console.log("Fetched data", data);
         return data;
     } catch (error) {
         console.error("Error fetching data:", error);
